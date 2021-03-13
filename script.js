ef9e92be5480efaa2f78a8902d2dec5dc146ff7a
Joey <joewoodnyc@gmail.com>
2021-03-13 14:46:09 -0500

    working js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 // for MusixMatch API search, so can search by LYRICS or ARTIST or SONG
 const musixMatchApiKey = '07aa5ecd02d38ca088c3bbcb58ebaf53';
-const searchURL = 'https://api.musixmatch.com/ws/1.1/track.search';
-const trackURL = 'https://api.musixmatch.com/ws/1.1/track.lyrics.get';
-
-// link to MusixMatch artist search results
-// const linkURL = 'https://www.musixmatch.com/search';
-
-//https://www.musixmatch.com/search/i%20just%20ride/lyrics
-
-// link to MusixMatch lyrics search results
-const linkURL = 'https://www.musixmatch.com/lyrics';
 
 //youtube API
 const youtubeApiKey = 'AIzaSyBKXGaS0dwFOXnn1DhiFpKrE33E-k8veB4';
@@ -21,24 +11,61 @@ const results = document.getElementById('results')
 
 searchBtn.addEventListener('click', lyricSearch)
 //.body.message.tracklist
+
 function lyricSearch() {
     results.classList.remove("hidden");
     var userInput = inputVal()
     var musixmatchApiURL = 'https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=' + userInput + '&page_size=5&page=1&s_track_rating=desc&apikey=' + musixMatchApiKey
     // setLoading(true);
     fetch (musixmatchApiURL)
-    .then(function(res){ return res.json() })
+    .then(function(res){ 
+        console.log(res)
+       return res.json() })
     .then(function(data){
         // setLoading(false);
         var tracklist = data.message.body.track_list;
         console.log(tracklist);
-        tracklist[i].track
+        //tracklist[i].track
     })
 }
 
 function inputVal() {
         return document.getElementById("searchbar").value;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=justin bieber&page_size=3&page=1&s_track_rating=desc&apikey=
 
@@ -48,7 +75,16 @@ function inputVal() {
 
 
 
+// const searchURL = 'https://api.musixmatch.com/ws/1.1/track.search';
+// const trackURL = 'https://api.musixmatch.com/ws/1.1/track.lyrics.get';
 
+// // link to MusixMatch artist search results
+// // const linkURL = 'https://www.musixmatch.com/search';
+
+// //https://www.musixmatch.com/search/i%20just%20ride/lyrics
+
+// // link to MusixMatch lyrics search results
+// const linkURL = 'https://www.musixmatch.com/lyrics';
 
 
 
